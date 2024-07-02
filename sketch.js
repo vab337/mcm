0b890a95930c74403797eb936b1aa8c01906b1c2
blanklounge <ven@thelabsaigon.com>
2024-07-03 00:45:30 +0700

    milk slider

---
+++ sketch.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const sliders = document.querySelectorAll('.slider');
+  
+  sliders.forEach(slider => {
+      slider.addEventListener('input', () => {
+          updateImage(slider);
+      });
+  });
+});
+
+function updateImage(slider) {
+  const imageContainer = document.getElementById('imageContainer');
+  const sliders = document.querySelectorAll('.slider');
+  
+  sliders.forEach(s => {
+    const label = s.previousElementSibling;
+    if (s === slider) {
+        s.style.opacity = '1';
+        label.style.opacity = '1';
+    } else {
+        s.style.opacity = '0.5';
+        label.style.opacity = '0.5';
+    }
+});
+
+  let imageName = '';
+  switch (slider.name) {
+      case 'colors':
+          imageName = `assets/color${slider.value}.png`;
+          break;
+      case 'logoSize':
+          imageName = `assets/logo${slider.value}.png`;
+          break;
+      case 'information':
+          imageName = `assets/info${slider.value}.png`;
+          break;
+      case 'photoToIllustration':
+          imageName = `assets/photo${slider.value}.png`;
+          break;
+      case 'copy':
+          imageName = `assets/copy${slider.value}.png`;
+          break;
+  }
+  
+  imageContainer.style.backgroundImage = `url(${imageName})`;
+}
